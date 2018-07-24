e830847b3519ad34a6cdd9ccb88e5c41107bd85a
lucas <lucas.depetris@awsoftware.com.ar>
2018-07-24 10:12:43 -0300

    - Navigation

--- App.js
+++ App.js
@@ -1,72 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native';
-import { Alert, AppRegistry, Button ,TouchableOpacity,ScrollView } from 'react-native';
+// App.js
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import Settings from './screens/Settings';
+import Home from './screens/Home';
 
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
+const AppNavigator = createStackNavigator({
+  SettingScreen: { screen: Settings },
+  HomeScreen: { screen: Home }
+});
 
+export default class App extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-
     return (
-      <View style={styles.container}>
-        
-        
-          <View style={styles.contTextos}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <Text>Changes you make will automatically reload.</Text>
-            <Text>Shake your phone to open the developer menu.</Text>
-            <Image source={pic} style={{width: 193, height: 110}}/>
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button
-              style={styles.loginScreenButton}
-              onPress={this._onPressButton}
-              title="Press Me"
-            />
-            <TouchableOpacity onPress={this._onPressButton}>
-              <View style={styles.button}>
-                <Text style={styles.buttonText}>TouchableOpacity idoolo lucas capo genio idolo idolo idolo idolo</Text>
-              </View>
-          </TouchableOpacity>
-          </View>
-        
-      </View>
+      <AppNavigator />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:40,
-  },
-  contTextos:{
-    justifyContent:'center',
-    alignItems:'center'
-  },
-  buttonContainer: {
-    width:'100%',
-    backgroundColor:'#eee',
-    alignItems:'center'
-   },
-  button: {
-    width:'100%',
-    marginBottom: 30,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  }
-});

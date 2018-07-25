1e2332a1d0a9439ae6aa5545cd76bb043002f99a
lucas <lucas.depetris@awsoftware.com.ar>
2018-07-25 10:30:14 -0300

    -Add axios library

--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,67 @@
 // Home.js
 
 import React, { Component } from 'react';
-import { Alert,ScrollView,View, Text , Button, TouchableOpacity,TouchableWithoutFeedback, TextInput, StyleSheet,Keyboard,FlatList} from 'react-native';
+import { Alert,ScrollView,View, Text , Button, TouchableOpacity,
+        TouchableWithoutFeedback, TextInput, StyleSheet,
+        Keyboard,FlatList,ActivityIndicator} from 'react-native';
+import axios from 'axios';
+
+var reqCancelRequest = axios.CancelToken.source();
 
 export class Home extends Component {
   state = {
-    string:''
+    string:'',
+    loading:false
+  }
+
+  componentDidMount(){
+    console.log('1. componentDidMount')
+    reqCancelRequest = axios.CancelToken.source();
   }
 
   _onPressButton = () => {
-    Alert.alert(this.state.string)
+    this.setState({loading:true})
+    this._getProductosByString(this.state.string)
   }
 
   _dismissKeyboard = () =>{
     Keyboard.dismiss()
   }
 
+  _getProductosByString = (producto) => {
+    
+    axios.get(`https://d735s5r2zljbo.cloudfront.net/prod/productos?string=${producto}&lat=-34.6012424&lng=58.377395&limit=10`, {
+      cancelToken: reqCancelRequest.token
+    })
+    .then(response => {
+      console.log(response.data)
+      this.setState({loading:false})
+    })
+    .catch(thrown => {
+      if (axios.isCancel(thrown)) {
+        console.log('Request canceled', thrown.message);
+        this.setState({loading:false})
+      } else {
+        // handle error
+        console.log(thrown)
+        this.setState({loading:false})
+      }
+      
+    }); 
+  }
+
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
+
+    reqCancelRequest.cancel('Operation canceled by the user.');
+    reqCancelRequest = null
+  }
+
   render() {
+    if(this.state.loading){
+      return <ActivityIndicator size="large" color="#0000ff" />
+    }
+    
     return (
       <ScrollView>
         
